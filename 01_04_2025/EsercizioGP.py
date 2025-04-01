d3a355a277f1153f4dbb9baec56e39ba73067f26
Stefano Raimondi <stefanoraimondi.pl@gmail.com>
2025-04-01 14:39:29 +0200

    Delete 01_04_2025 directory

--- 01_04_2025/EsercizioGP.py
+++
@@ -1,9 +0,0 @@
-numero_utente = int(input("Scegli un numero da 1 a 10: "))
-
-numero_segretissimo = 3
-
-if numero_utente == numero_segretissimo:
-    print("Il numero è corretto")
-else:
-    print("Il numero non è corretto")
-    print("Riprova")
